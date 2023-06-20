b85620e2c8b9c0825f76df8ef1f21a7cd8ab11fd
Serhii <themaulmartin@gmail.com>
2023-06-20 18:54:38 +0300

    Added git ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import PyPDF2
 import os
-import re  # re.search(string, text)
 import json
 from sqlalchemy.orm import Session
 from sqlalchemy import select
@@ -12,71 +11,126 @@ rulesList = []
 fileName = "rules.json"
 pathToIndexes = "G:/Мій диск/Warhammer/10th"
 
-files = list(filter(lambda x: "Index" in x, os.listdir(pathToIndexes)))
 
-for f in files:
-    armyIndex = {"name": f.split('.')[0], "rules": [], "units": []}
-    reader = PyPDF2.PdfReader(pathToIndexes + "/" + f)
-    numPages = len(reader.pages)
-    isFront = True
-    data_cards = {"front": {}, "back": {}, "name": {}, "keywords": {}, "points_value": [{"models": 1, "points": 1}]}
-    for p in reader.pages:
-        if p.extract_text() == '':
-            print("Art Page: Skipping")
-        elif 'ARMOURY' in p.extract_text():
-            print("Armoury Page: Skipping")
-        elif p.mediabox.width < 500:
-            # Rules page
-            armyIndex["rules"].append(p.extract_text())
-        else:
-            # data card
-            if isFront:
-                data_cards["front"] = p.extract_text()
-                dc_name = p.extract_text().split('KEYWORDS')[0].strip()
-                if '\n' in dc_name:
-                    dc_name = dc_name.split('\n')[0]
-                data_cards["name"] = dc_name
-                print(f"Processing: {armyIndex['name']}/{data_cards['name']}")
-                isFront = not isFront
+def Scan(rulesList, fileName, pathToIndexes):
+    files = list(filter(lambda x: "Index" in x, os.listdir(pathToIndexes)))
+
+    if files.count == 0:
+        files = list(filter(lambda x: "Index" in x, os.listdir("/indexes")))
+
+    for f in files:
+        armyIndex = {
+            "name": f.split(".")[0].replace("Index", "").strip(),
+            "rules": [],
+            "units": [],
+        }
+        reader = PyPDF2.PdfReader(pathToIndexes + "/" + f)
+        numPages = len(reader.pages)
+        isFront = True
+        data_cards = {
+            "front": {},
+            "back": {},
+            "name": {},
+            "keywords": {},
+            "points_value": [{"models": 1, "points": 1}],
+        }
+        for p in reader.pages:
+            if p.extract_text() == "":
+                print("Art Page: Skipping")
+            elif "ARMOURY" in p.extract_text():
+                print("Armoury Page: Skipping")
+            elif p.mediabox.width < 500:
+                # Rules page
+                armyIndex["rules"].append(p.extract_text())
             else:
-                data_cards["back"] = p.extract_text()
-                s1 = p.extract_text()
-                s2 = s1.split('KEYWORDS')[1]
-                s3 = s2.replace(':','')
-                s4 = s3.split('\n')[0]
-                data_cards["keywords"] = s4.replace('FACTION','').strip()
-                isFront = not isFront
-                armyIndex["units"].append(data_cards.copy())
-                data_cards["front"] = {}
-                data_cards["back"] = {}
-    rulesList.append(armyIndex)
-
-if os.path.exists(fileName):
-    os.remove(fileName)
-    print('File exists, replacing')
-
-jsonRules = open(fileName, "x")
-jsonRules.write(json.dumps(rulesList, indent=4).replace('\u2013', '-').replace('\n', ' '))
- 
-# engine = db_connect()
-# with Session(engine) as session:
-#     for faction in rulesList:
-#         f = Faction(z
-#         )
-
-#         session.add(f)
-
-#         db_f = select(Faction).where(Faction.name == faction.name)
-
-#         units_list = []
-
-#         for unit in faction.units:
-#             units_list.append(Unit(
-#                 __name__ = ''
-#             ))
+                # data card
+                if isFront:
+                    data_cards["front"] = p.extract_text()
+                    dc_name = p.extract_text().split("KEYWORDS")[0].strip()
+                    if "\n" in dc_name:
+                        dc_name = dc_name.split("\n")[0]
+                    data_cards["name"] = dc_name
+                    print(f"Processing: {armyIndex['name']}/{data_cards['name']}")
+                    isFront = not isFront
+                else:
+                    data_cards["back"] = p.extract_text()
+                    s1 = p.extract_text().replace("\n", "")
+                    s2 = s1.split("KEYWORDS")[1]
+                    s3 = s2.replace(":", "")
+                    s4 = s3.split("\n")[0]
+                    data_cards["keywords"] = s4.replace("FACTION", "").strip()
+                    isFront = not isFront
+                    armyIndex["units"].append(data_cards.copy())
+                    data_cards["front"] = {}
+                    data_cards["back"] = {}
+        rulesList.append(armyIndex)
+    if os.path.exists(fileName):
+        os.remove(fileName)
+        print("File exists, replacing")
+    jsonRules = open(fileName, "x")
+    jsonRules.write(
+        json.dumps(rulesList, indent=4).replace("\u2013", "-").replace("\n", " ")
+    )
 
 
+def Upload(rulesList):
+    engine = db_connect()
+    with Session(engine) as session:
+        for faction in rulesList:
+            db_f = session.execute(
+                select(Faction).where(Faction.name == faction["name"])
+            ).first()
+
+            if db_f is None:
+                f = Faction(name=faction["name"])
+                session.add(f)
+                session.commit()
+                db_f = session.execute(
+                    select(Faction).where(Faction.name == faction["name"])
+                ).first()
+            db_f = db_f[0]
+
+            print(f"{db_f.faction_id}. {db_f.name}")
+
+            for unit in faction["units"]:
+                db_u = session.execute(
+                    select(Unit).where(Unit.name == unit["name"])
+                ).first()   
+
+                if db_u is None:
+                    u = Unit(
+                        name=unit["name"],
+                        faction_id=db_f.faction_id,
+                        keywords=unit["keywords"],
+                        meta=unit["front"] + "-(|)-" + unit["back"],
+                    )
+                    session.add(u)
+                    session.commit()
 
 
+sel = -1
 
-print("Finished")
+while sel != "0":
+    print("Select Mode:")
+    print("1. Scan")
+    print("2. Upload")
+    print("3. Scan and Upload")
+    print("0. Exit")
+    sel = input("> ")
+    if sel == "1":
+        Scan(rulesList, fileName, pathToIndexes)
+    elif sel == "2":
+        if rulesList == []:
+            print("Loading data from file")
+            f = open(fileName, "r")
+            rulesList = json.load(f)
+        Upload(rulesList)
+    elif sel == "3":
+        Scan(rulesList, fileName, pathToIndexes)
+        Upload(rulesList)
+    elif sel == "0":
+        exit
+    else:
+        print("Invalid Input")
+    d = input("Press 'Enter' key to continue")
+    os.system("cls")
